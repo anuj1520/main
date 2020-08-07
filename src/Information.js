fcd55eb63359177d56f984198ab6d373b7662a43
anuj1520 <anujalerts15@gmail.com>
2020-08-08 02:23:19 +0530

    Changing Information

--- src/Information.js
+++ src/Information.js
@@ -2,8 +2,8 @@ export const details = [
   {
     id: 1,
     name: 'Anuj Sharma',
-    intro:'I amd currently pursuing btech from UPES in specialization with Cyber Security And forensics.Apart from Cyber Security part,i am developer too.I use React Framework for FrontEnd Development',
-    about:'A paragraph is a series of related sentences developing a central idea, called the topic Try to think about paragraphs in terms of thematic unity a paragraph is a sentence or a group of sentences that supports one central unified idea. Paragraphs add one idea at a time to your broader argument.',
+    intro:'I am currently pursuing btech from UPES in specialization with Cyber Security And forensics.Apart from Cyber Security part,I am developer too.I use React Framework for FrontEnd Development',
+    about:'I am from Lucknow .I love travelling from places to places and explore things. ',
     git:'https://github.com/anuj1520',
     fb:'https://www.facebook.com/people/Anuj-Sharma/100025020051521',
     insta:'',
@@ -23,7 +23,7 @@ export const details = [
 
     PLink1:'https://github.com/anuj1520/Robo',
     PLink2:'https://github.com/anuj1520/Basic-Shopping-Cart',
-    PLink3:'https://github.com/anuj1520/Portfolio'
+    PLink3:'https://github.com/anuj1520/main'
 
    }
   ];
